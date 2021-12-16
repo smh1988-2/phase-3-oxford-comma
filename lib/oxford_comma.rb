5568de23e4d41e412df5caba07e98608d253860f
smh1988-2 <seamhurley1988@gmail.com>
2021-12-15 20:16:10 -0800

    commit tree f79a7cfd602307caa95cab9321a85f1ea706a4b2

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    else
+        new_arr =[]
+        i = 0
+        while i < array.length - 1
+            new_arr << array[i] + ","
+            i = i+1
+        end
+        new_arr << "and" + array[-1]
+        new_arr.join(" ")
+    end
 end
+
+oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])
